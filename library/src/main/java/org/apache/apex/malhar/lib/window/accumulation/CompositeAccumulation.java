bab187fbf4c63c5e100f53f6af0e9de2438b0958
brightchen <bright@datatorrent.com>
2017-02-24 11:10:44 -0800

    APEXMALHAR-2395: clean up code

--- library/src/main/java/org/apache/apex/malhar/lib/window/accumulation/CompositeAccumulation.java
+++ library/src/main/java/org/apache/apex/malhar/lib/window/accumulation/CompositeAccumulation.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.apex.malhar.lib.window.accumulation;
 
 import java.util.List;
@@ -39,7 +57,7 @@ public class CompositeAccumulation<InputT> implements Accumulation<InputT, List,
   public List defaultAccumulatedValue()
   {
     List defaultValues = Lists.newArrayList();
-    for(Accumulation accumulation: accumulations) {
+    for (Accumulation accumulation : accumulations) {
       defaultValues.add(accumulation.defaultAccumulatedValue());
     }
     return defaultValues;
@@ -49,11 +67,11 @@ public class CompositeAccumulation<InputT> implements Accumulation<InputT, List,
   @Override
   public List accumulate(List accumulatedValues, InputT input)
   {
-    for(int index = 0; index < accumulations.size(); ++index) {
+    for (int index = 0; index < accumulations.size(); ++index) {
       Accumulation accumulation = accumulations.get(index);
       Object oldValue = accumulatedValues.get(index);
       Object newValue = accumulation.accumulate(oldValue, input);
-      if(newValue != oldValue) {
+      if (newValue != oldValue) {
         accumulatedValues.set(index, newValue);
       }
     }
@@ -63,8 +81,9 @@ public class CompositeAccumulation<InputT> implements Accumulation<InputT, List,
   @Override
   public List merge(List accumulatedValues1, List accumulatedValues2)
   {
-    for(int index = 0; index < accumulations.size(); ++index) {
-      accumulatedValues1.set(index, accumulations.get(index).merge(accumulatedValues1.get(index), accumulatedValues2.get(index)));
+    for (int index = 0; index < accumulations.size(); ++index) {
+      accumulatedValues1.set(index,
+          accumulations.get(index).merge(accumulatedValues1.get(index), accumulatedValues2.get(index)));
     }
     return accumulatedValues1;
   }
